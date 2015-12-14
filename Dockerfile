9dc161cc0c1e45f60a4c72a640a398bce0db12f3
Phil Ngo <ngo.phil@gmail.com>
2015-12-14 20:34:37 +0000

    Modify docker files, add static root, update requirements (gunicorn)

--- Dockerfile
+++ Dockerfile
@@ -4,20 +4,21 @@ ENV PYTHONUNBUFFERED 1
 
 RUN apt-get update && apt-get install -y postgresql-client libpq-dev
 
+EXPOSE 8000
+
+ENV DJANGO_SETTINGS_MODULE oeem_client.settings
+ENV STATIC_ROOT /srv/static
+
+RUN mkdir /srv/static /srv/logs
+
+VOLUME /srv/static
+
+
 RUN mkdir /code
 WORKDIR /code
 ADD requirements.txt /code/
 RUN pip install -r requirements.txt
 ADD . /code/
-
-ENV DJANGO_SETTINGS_MODULE oeem-client.settings
-ENV STATIC_ROOT /srv/static
-
-EXPOSE 8000
-
-RUN mkdir /srv/static /srv/logs
-
-VOLUME /srv/static
 
 COPY ./docker-entrypoint.sh /
 ENTRYPOINT ["/docker-entrypoint.sh"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-export DATABASE_URL=postgres://$(cat /etc/secret-volume/pg-user):$(cat /etc/secret-volume/pg-password)@postgres:5432/$(cat /etc/secret-volume/pg-user)
+export DATABASE_URL=postgres://$(cat /etc/secret-volume/pg-user):$(cat /etc/secret-volume/pg-password)@$(cat /etc/secret-volume/pg-service-name).default.svc.cluster.local:5432/$(cat /etc/secret-volume/pg-user)
 export SECRET_KEY=$(cat /etc/secret-volume/secret-key)
-export CLIENT_ID=$(cat /etc/secret-volume/client-id)
 export DATASTORE_URL=$(cat /etc/secret-volume/datastore-url)
 export DATASTORE_ACCESS_TOKEN=$(cat /etc/secret-volume/datastore-access-token)
 
 if [ -z "$SETUP" ]; then
 	echo "Skipping setup because SETUP is unset"
 else
+    echo $DATABASE_URL
 	python manage.py migrate
 	python manage.py collectstatic --noinput
 fi
@@ -17,5 +17,5 @@ touch /srv/logs/gunicorn.log
 touch /srv/logs/access.log
 tail -n 0 -f /srv/logs/*.log &
 
-exec gunicorn oeem-client.wsgi \
+exec gunicorn oeem_client.wsgi \
     --bind 0.0.0.0:8000
