59230be23b0662717283ecbdc5f140ce315489a9
Phil Ngo <ngo.phil@gmail.com>
2016-01-05 13:51:01 -0600

    Add env script

---
+++ env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export DATABASE_URL=postgres://$(cat /etc/secret-volume/pg-user):$(cat /etc/secret-volume/pg-password)@$(cat /etc/secret-volume/pg-service-name).default.svc.cluster.local:5432/$(cat /etc/secret-volume/pg-user)
+export SECRET_KEY=$(cat /etc/secret-volume/secret-key)
+export DATASTORE_URL=$(cat /etc/secret-volume/datastore-url)
+export DATASTORE_ACCESS_TOKEN=$(cat /etc/secret-volume/datastore-access-token)
