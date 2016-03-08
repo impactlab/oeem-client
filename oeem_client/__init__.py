95eef158b21071a84c4cad9403705fffa75af61c
Phil Ngo <ngo.phil@gmail.com>
2016-03-08 14:35:48 -0600

    Add version information

---
+++ oeem_client/__init__.py
@@ -0,0 +1,8 @@
+__title__ = 'Open Energy Efficiency Meter Client Dashboard'
+__version__ = '0.1.0'
+__author__ = 'Phil Ngo'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2011-2016 Open Energy Efficiency'
+
+# Version synonym
+VERSION = __version__
